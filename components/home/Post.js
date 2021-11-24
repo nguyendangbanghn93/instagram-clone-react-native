a6c620588ca5948826100957a888eff7dac7bcf0
nguyendangbanghn93 <nguyendangbanghn@gmail.com>
2021-11-25 06:15:01 +0700

    .

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,18 +1,102 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { Divider } from 'react-native-elements';
 
+const postFooterIcons = [
+    {
+        name: "Like",
+        imageUrl: "https://img.icons8.com/ios/50/ffffff/facebook-like--v1.png",
+    },
+    {
+        name: "Comment",
+        imageUrl: "https://img.icons8.com/ios/50/ffffff/speech-bubble--v1.png",
+    },
+    {
+        name: "Share",
+        imageUrl: "https://img.icons8.com/ios/50/ffffff/paper-plane.png",
+    },
+    {
+        name: "Save",
+        imageUrl: "https://img.icons8.com/ios/50/ffffff/bookmark-ribbon--v1.png",
+    }
+]
 export default function Post({ post }) {
 
     return (
         <View style={styles.container}>
             <Divider width={1} orientation="vertical" />
+            <PostHeader post={post} />
+            <PostImage post={post} />
+            <PostFooter />
         </View>
     )
 }
+
+const PostHeader = ({ post }) => (
+    <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: 5,
+        alignItems: 'center'
+    }}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Image source={{
+                uri: post.profile_picture
+            }}
+                style={styles.story} />
+            <Text style={{
+                color: "white",
+                marginLeft: 5,
+                fontWeight: "700"
+            }}>{post.user}</Text>
+        </View>
+        <Text style={{
+            color: "white",
+            fontWeight: "900"
+        }}>...</Text>
+    </View>
+)
+
+const PostImage = ({ post }) => (
+    <View style={{ width: "100%", height: 450 }}>
+        <Image
+            source={{ uri: post.imageUrl }}
+            style={{ height: "100%", resizeMode: "cover" }}
+        />
+    </View>
+)
+const PostFooter = () => (
+    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <View style={styles.leftFooterIconsContainer}>
+            <Image style={styles.footerIcon} source={{ uri: postFooterIcons[0].imageUrl }} />
+            <Image style={styles.footerIcon} source={{ uri: postFooterIcons[1].imageUrl }} />
+            <Image style={[styles.footerIcon]} source={{ uri: postFooterIcons[2].imageUrl }} />
+        </View>
+    </View>
+)
 
 const styles = StyleSheet.create({
     container: {
         marginBottom: 30,
     },
+    story: {
+        width: 35,
+        height: 35,
+        borderRadius: 50,
+        marginLeft: 6,
+        borderWidth: 1.6,
+        borderColor: '#ff8501'
+    },
+    footerIcon: {
+        width: 33,
+        height: 33,
+    },
+    shareIcon: {
+        transform: [{ rotate: "320deg" }]
+    },
+    leftFooterIconsContainer: {
+        flexDirection: "row",
+        width: "32%",
+        justifyContent: "space-between",
+    }
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import Header from '../components/home/Header';
 import Post from '../components/home/Post';
 import Stories from '../components/home/Stories';
-
+import { POSTS } from '../data/posts'
 export default function HomeScreen() {
 
     return (
         <SafeAreaView style={styles.container}>
-           <Header/>
-            <Stories/>
-            <Post/>
+            <Header />
+            <Stories />
+            <ScrollView>
+                {POSTS.map((post, index) => (
+                    <Post post={post} key={index} />
+                ))}
+            </ScrollView>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:"black"
+    container: {
+        flex: 1,
+        backgroundColor: "black"
     }
 });
